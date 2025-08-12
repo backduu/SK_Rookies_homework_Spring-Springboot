e0054c41baa99aa771398143be24696cb4825721
backduu <power4290@gmail.com>
2025-08-12 13:13:49 +0900

    [Spring 실습3] Dependency Injection 전략3 – Java Configuration 방식으로 설정하기

--- src/test/java/mylab/notification/di/annot/NotificationConfigTest.java
+++ src/test/java/mylab/notification/di/annot/NotificationConfigTest.java
@@ -26,7 +26,26 @@ public class NotificationConfigTest {
         // NotificationManager가 주입되었는지 확인
         assertNotNull(notificationManager, "NotificationManager가 null입니다.");
 
-        // EmailNotificationService가 주입되었는지 확인
-        assertNotNull(notificationManager.getEmailService(), "EmailNotificationService가 null입니다.");
+        //이메일 서비스 검증
+        NotificationService emailService = notificationManager.getEmailService();
+        assertNotNull(notificationManager.getEmailService(), "EmailNotificationService is null");
+        assertTrue(emailService instanceof EmailNotificationService, "EmailService is not type of EmailNotificationService");
+
+        EmailNotificationService email = (EmailNotificationService) emailService;
+        assertEquals("smtp.mail.com", email.getSmtpServer(), "SMTP is 'smtp.mail.com'");
+        assertEquals(587, email.getPort(), "SMTP port number is 587");
+        
+        // SMS 서비스 검증
+        NotificationService smsService = notificationManager.getSmsService();
+        assertNotNull(smsService, "SmsNotificationService is null");
+        assertTrue(smsService instanceof SmsNotificationService, "SmsService is not type of SmsNotificationService");
+
+        SmsNotificationService sms = (SmsNotificationService) smsService;
+        assertEquals("SKT", sms.getProvider(), "Provider is not 'SKT'");
+        
+        // NotificationManager의 메서드 실행
+        notificationManager.sendNotificationByEmail("테스트 이메일");
+        notificationManager.sendNotificationBySms("테스트 SMS");
+
     }
 }
